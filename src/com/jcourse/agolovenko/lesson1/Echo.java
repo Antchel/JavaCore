feb3876e4b89bdf34dc98b4322a8d23e8225386d
Anton Golovenko <anton.golovenko@vmssoftware.com>
2022-04-21 14:37:10 +0500

    Echo fixes.

--- src/com/jcourse/agolovenko/lesson1/Echo.java
+++ src/com/jcourse/agolovenko/lesson1/Echo.java
@@ -14,10 +14,9 @@ public class Echo {
     public void executeEcho() throws IOException {
 
         if (System.console() != null) {
-            byte[] byteStr;
-            System.out.println(System.console().charset().name());
-            byteStr = System.console().readLine().getBytes(System.console().charset().name());
-            System.out.println(new String(byteStr,System.console().charset().name()));
+            String consoleCharset = System.console().charset().name();
+            System.out.println(consoleCharset);
+            System.out.println(new String(System.console().readLine().getBytes(consoleCharset),consoleCharset));
         } else {
             System.out.println(System.getProperty("file.encoding"));
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
